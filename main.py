6937ed62258f6fe2c81c6002ded82a69bfa4ef71
Chirag Patel <cyp5@njit.edu>
2021-11-08 21:45:08 -0500

    Objection detection now working in real time

--- main.py
+++ main.py
@@ -22,7 +22,12 @@
 
 import cv2 #importing the library
 
-image = cv2.imread('images/animal.png') #imread() reads the image from the file specified by filename
+thres = 0.5 # Threshold to detect object
+
+cap = cv2.VideoCapture(0) # To get camera access
+# Setting certain parameters on how big our image is.
+cap.set(3, 640)
+cap.set(4, 480)
 
 classNames = [] #empty array that will store all the dataset information
 classFile = 'coco.names'
@@ -43,17 +48,19 @@ net.setInputScale(1.0/ 127.5)
 net.setInputMean((127.5, 127.5, 127.5))
 net.setInputSwapRB(True)
 
-# This will send our image to our model and then it will give us the predictions.
-classIds, confs, bbox = net.detect(image,confThreshold=0.5)
-print(classIds, bbox)
+while True:
+    success,image = cap.read() # Gives us our image
+    # This will send our image to our model and then it will give us the predictions.
+    classIds, confs, bbox = net.detect(image,confThreshold=thres)
+    print(classIds, bbox)
 
+    if len(classIds)!= 0:
+    # This is to set up what the drawn box size and colour is and the font/size/colour of the name tag will be
+        for classId, confidence, box in zip(classIds.flatten(),confs.flatten(),bbox):
+            cv2.rectangle(image,box,color=(0,0,255),thickness=2) # To create a rectangle around the detected image with color red and thickness of 2
+            cv2.putText(image,classNames[classId-1].upper(),(box[0]+10,box[1]+30), # This adds a text around the rectangle which writes the detected name of the object
+                    cv2.FONT_HERSHEY_TRIPLEX,1,(0,0,255),2) # Chooses the font and color of the text
 
-# This is to set up what the drawn box size and colour is and the font/size/colour of the name tag will be
-for classId, confidence, box in zip(classIds.flatten(),confs.flatten(),bbox):
-    cv2.rectangle(image,box,color=(0,0,255),thickness=2) # To create a rectangle around the detected image with color red and thickness of 2
-    cv2.putText(image,classNames[classId-1].upper(),(box[0]+10,box[1]+30), # This adds a text around the rectangle which writes the detected name of the object
-            cv2.FONT_HERSHEY_TRIPLEX,1,(0,0,255),2) # Chooses the font and color of the text
-
-cv2.imshow("Output", image) #To show the output image with the results
-cv2.waitKey(0) # Displays the output window infinitely
+    cv2.imshow("Output", image) #To show the output image with the results
+    cv2.waitKey(1) # Displays the output window infinitely
 
